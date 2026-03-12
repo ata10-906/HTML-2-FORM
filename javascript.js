333f3dea13bd7d0f65342da837847a1733d18b89
ata10-906 <atangtselakgosi8@gmail.com>
2026-03-12 16:12:03 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
+<script>
+  fuction validateForm() {
+      let Username = document.forms["myForm"]["username"].value
+      let Email = document.forms["myForm"]["email"].value
+      let Telephone = document.forms["myForm"]["telephone"].value
+      let Position = document.forms["myForm"]["job position"].value
 
+      if(Username == ""){
+            alert("Please enter your name");
+                  return false;
+        
+      }
+
+      if(Email == ""){
+            alert("Please enter your email address");
+                  return false;
+        
+      }
+
+      if(Telephone == ""){
+            alert("Please enter your telephone number");
+                  return false;
+        
+      }
+
+      if(Position == ""){
+            alert("Please select a job position");
+                  return false;
+        
+      }
+  }
